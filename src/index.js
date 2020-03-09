0d404956670b02ea86add1e9c4f6b6c9c9ca7d30
Olga Bainova (nb) <terra.456@yandex.ru>
2020-03-10 04:28:10 +0500

    feat: implement function that reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
+    
+    if (!n || n == 0) {
+        return 0;
+      } else {
+        // var absN = Math.abs(n);
+        // var nStr = String(absN);
+        // var newStrArr = Array.from(nStr);
+        // var reverseArr = newStrArr.reverse();
+        // var reverseStr = reverseArr.join('');
+        
+        return Array.from(String(Math.abs(n))).reverse().join('');
+      }
   
 }
